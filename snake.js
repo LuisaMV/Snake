239c923aae80ea465ea76eb1cf3a66e5c880dbe1
Alberto Romero Navia <lguitarras0594@gmail.com>
2017-07-03 20:50:23 -0500

    implement random sprinkle creation

--- snake.js
+++ snake.js
@@ -1,17 +1,27 @@
 function createBoard() {
-	var root = document.getElementById('root');
-	console.log(root);
-	for (var i = 0; i < 50; i++) {
-		var unit = document.createElement('div');
-		unit.classList.add('flex');
-		for (var j = 0; j < 50; j++) {
-			var child = document.createElement('div');
-			child.classList.add('unit');
-			unit.appendChild(child);
-		}
-		root.appendChild(unit);
-	}
+ 	 var root = document.getElementById('root');
+   var board = [];
+	 for (var i = 0; i < 50; i++) {
+     var row = [];
+		 var unit = document.createElement('div');
+		 unit.classList.add('flex');
+		 for (var j = 0; j < 50; j++) {
+			 var child = document.createElement('div');
+			 child.classList.add('unit');
+			 unit.appendChild(child);
+       row.push(child);
+		 }
+		 root.appendChild(unit);
+     board.push(row);
+	 }
+   return board;
 }
 
-createBoard();
+function createRandomSprinkle(board) {
+  var randomRow = Math.floor(Math.random() * 50);
+  var randomColumn = Math.floor(Math.random() * 50);
+  board[randomRow][randomColumn].classList.add('sprinkle');
+}
 
+var board = createBoard();
+createRandomSprinkle(board);
